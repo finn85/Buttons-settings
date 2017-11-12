63d8989746a110f092b519fc25ca7d506bfac106
Finenko Artem <30906419+finn85@users.noreply.github.com>
2017-11-12 12:31:11 +0300

    Update mainscript.js

--- mainscript.js
+++ mainscript.js
@@ -1,42 +1,43 @@
 var butTextVal,
-	butFontSizeVal = document.getElementById("buttonFontSize").value,
-	butHueVal = document.getElementById("buttonHue").value,
-	butSatVal = document.getElementById("buttonSaturation").value,
-	butLightVal = document.getElementById("buttonLightness").value,
-	butHeightVal = document.getElementById("buttonHeight").value,
-	butWidthVal = document.getElementById("buttonWidth").value;
+	butFontSizeVal,
+	butHueVal,
+	butSatVal,
+	butLightVal,
+	butHeightVal,
+	butWidthVal;
 //---------------------------------------------------------------------------------------------------------------------------------------------------------------
 window.onload = function() {
-	writeCSS();
-	loadPreviewStyle();
+	upWrWr();
 }
 
 buttonText.addEventListener("keyup",changeButText);
 
-buttonHue.addEventListener("input",changeButHue);
-buttonHue.addEventListener("input",writeCSS);
+buttonHue.addEventListener("input",upWrWr);
+buttonSaturation.addEventListener("input",upWrWr);
+buttonLightness.addEventListener("input",upWrWr);
+buttonFontSize.addEventListener("input",upWrWr);
+buttonHeight.addEventListener("input",upWrWr);
+buttonWidth.addEventListener("input",upWrWr);
 
-buttonSaturation.addEventListener("input",changeButSat);
-buttonSaturation.addEventListener("input",writeCSS);
+buttonType1.addEventListener("mousedown", function(){
+	changeButStyle(1);
+});
+buttonType1.addEventListener("mouseup", upWrWr);
 
-buttonLightness.addEventListener("input",changeButLight);
-buttonLightness.addEventListener("input",writeCSS);
+buttonType2.addEventListener("mousedown", function(){
+	changeButStyle(2);
+});
+buttonType2.addEventListener("mouseup", upWrWr);
 
-buttonFontSize.addEventListener("input",changeFontSize);
-buttonFontSize.addEventListener("input",writeCSS);
+buttonType3.addEventListener("mousedown", function(){
+	changeButStyle(3);
+});
+buttonType3.addEventListener("mouseup", upWrWr);
 
-buttonHeight.addEventListener("input",changeHeight);
-buttonHeight.addEventListener("input",writeCSS);
-
-buttonWidth.addEventListener("input",changeWidth);
-buttonWidth.addEventListener("input",writeCSS);
-
-button.addEventListener("mousedown", function () {
-	button.style.boxShadow = butBoxShadValClick();
-})
-button.addEventListener("mouseup", function() {
-	button.style.boxShadow = butBoxShadVal();
-})
+buttonType4.addEventListener("mousedown", function(){
+	changeButStyle(4);
+});
+buttonType4.addEventListener("mouseup", upWrWr);
 
 selectText.addEventListener("click", selectCssText);
 //---------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -44,48 +45,38 @@ function changeButText() {
 	butTextVal = document.getElementById("buttonText").value;
 	button.innerHTML = butTextVal;
 }
-function changeFontSize(argument) {
+function updateVars() {
 	butFontSizeVal = document.getElementById("buttonFontSize").value;
-	button.style.fontSize = butFontSizeVal + "px";
-}
-function changeButHue() {
 	butHueVal = document.getElementById("buttonHue").value;
-	button.style.background = butBackgrVal();
-	button.style.borderColor = butBordColVal();
-	button.style.boxShadow = butBoxShadVal();
-}
-function changeButSat() {
 	butSatVal = document.getElementById("buttonSaturation").value;
-	button.style.background = butBackgrVal();
-	button.style.borderColor = butBordColVal();
-	button.style.boxShadow = butBoxShadVal();
-}
-function changeButLight() {
 	butLightVal = document.getElementById("buttonLightness").value;
-	button.style.background = butBackgrVal();
-	button.style.borderColor = butBordColVal();
-	button.style.boxShadow = butBoxShadVal();
-}
-function changeHeight() {
 	butHeightVal = document.getElementById("buttonHeight").value;
-	button.style.height = butHeightVal + "px";
-	button.style.lineHeight = butHeightVal + "px";
-	button.style.borderWidth = butBordWidthVal();
-	button.style.borderRadius = butBordRadVal();
-	button.style.boxShadow = butBoxShadVal();
-}
-function changeWidth() {
 	butWidthVal = document.getElementById("buttonWidth").value;
-	button.style.width = butWidthVal + "px";
 }
-function writeCSS() {
-	cssCode.innerHTML = cssText();
+function writeCssPreview() {
+	cssCode.innerHTML = cssText();;
 }
-function loadPreviewStyle() {
+function writeStyleInHead() {
 	var headEl = document.getElementsByTagName("head")[0],
+		oldStyleEl = document.getElementsByTagName("style")[0],
 		newStyleEl = document.createElement("style");
 	newStyleEl.innerHTML = cssText();
+	if(oldStyleEl){
+		oldStyleEl.remove();
+	}
 	headEl.appendChild(newStyleEl);
+}
+function upWrWr() {
+	updateVars();
+	writeCssPreview();
+	writeStyleInHead();
+}
+function changeButStyle(num) {
+	var body = document.getElementsByTagName("body")[0],
+		oldelem = document.getElementsByTagName("script")[1],
+		newElem = document.createElement("script");
+	newElem.setAttribute("src", "button0" + num + ".js");
+	body.replaceChild(newElem,oldelem);
 }
 function selectCssText() {
 	var rng, sel;
